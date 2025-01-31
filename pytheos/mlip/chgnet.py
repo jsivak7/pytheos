3ceb0c9796dc8cc86fcbe0240e09bdb4043f95a4
Jacob Sivak <jacobsivak7@gmail.com>
2025-01-31 11:31:00 -0500

    updated chgnet module for commenting up run_relax

--- pytheos/mlip/chgnet.py
+++ pytheos/mlip/chgnet.py
@@ -3,9 +3,10 @@
 from ase import Atoms
 
 
-def run_chgnet_relax(
+def run_relax(
     structure: Atoms,
     max_force=0.050,
+    optimizer="FIRE",
     # TODO can you run constrained relaxations (i.e. like ISIF = 8 in VASP?)
 ):
     """
@@ -17,21 +18,27 @@ def run_chgnet_relax(
         structure (Atoms): ASE Atoms object for structure.
         max_force (float, optional): Force convergence criteria for relaxation in eV/Angstrom.
             - Slightly lower than CHGNet default of 100 meV/Angstrom, good middle ground
+        optimizer (string): ASE optimizer to use. Defaults to "FIRE"
+            - Sometimes faster relaxation can be achieved with other optimizers like "BFGS" - do your own testing!
+            - https://wiki.fysik.dtu.dk/ase/ase/optimize.html
+            - https://gpaw.readthedocs.io/devel/ase_optimize/ase_optimize.html
 
     Returns:
         tuple: (Atoms object of relaxed structure, total energy in eV/atom w/ MP2020Compatibility corrections, and list of magnetic moments)
     """
 
-    from chgnet.model import StructOptimizer
+    from chgnet.model.dynamics import StructOptimizer
     from chgnet.model.model import CHGNet
     from pymatgen.core import Structure
 
-    print(f"Running CHGNet relaxation.")
+    print(
+        f"\nRunning CHGNet relaxation with force criteria = {max_force} meV/Angstrom -->\n"
+    )
 
     s = Structure.from_ase_atoms(structure)
 
     chgnet = CHGNet.load()
-    relaxer = StructOptimizer()
+    relaxer = StructOptimizer(optimizer_class=optimizer)
 
     # using a large number of steps to ensure force convergence criteria is reached
     # seems that for HEOs sometimes the default of 500 can be reached
@@ -39,16 +46,16 @@ def run_chgnet_relax(
 
     # Pymatgen structure object, can be written to .vasp file
     s_final_pmg = result["final_structure"]
-    print(f"CHGNet relaxed structure -->\n{s_final_pmg}")
+    print(f"\nCHGNet relaxed structure -->\n\n{s_final_pmg}")
     s_final = s_final_pmg.to_ase_atoms()
 
     # to get predictions from CHGNet on relaxed structure
-    prediction = chgnet.predict_structure(final_structure)
+    prediction = chgnet.predict_structure(s_final_pmg)
 
     # eV/atom with MP2020Compatibility corrections
     energy = float(prediction["e"])
 
     # same order as inputted structure
-    magmoms = final_structure.site_properties["magmom"]
+    magmoms = s_final_pmg.site_properties["magmom"]
 
     return (s_final, energy, magmoms)
